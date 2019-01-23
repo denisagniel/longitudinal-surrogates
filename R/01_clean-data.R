9565363b8c7949dcbbd326ad5a54b11a6c392159
Denis Agniel <dagniel@rand.org>
2019-01-22 16:45:58 -0800

    Changing some variable names.

--- R/01_clean-data.R
+++ R/01_clean-data.R
@@ -9,7 +9,8 @@ dpp_data <- read_csv(here('data', 'dpp.data.csv')) %>%
 ### make exclusions to for now focus on placebo vs lifestyle
 
 dpp_lp <- dpp_data %>%
-  filter(treatment_group %in% c('Lifestyle', 'Placebo'))
+  filter(treatment_group %in% c('Lifestyle', 'Placebo')) %>%
+  mutate(a = if_else(treatment_group == 'Lifestyle', 1, 0))
 
 #-----
 ## reshape the data
@@ -18,14 +19,14 @@ dpp_lp <- dpp_data %>%
 dpp_lp_long_t <-
   dpp_lp %>%
   select(release_id, matches('HBA1_._time')) %>%
-  gather(time_n, time, -release_id) %>%
+  gather(time_n, tt, -release_id) %>%
   separate(time_n, into = c('measure', 'time_n', 'tv')) %>%
   select(-tv)
 
 dpp_lp_long_v <-
   dpp_lp %>%
   select(release_id, matches('HBA1_._value')) %>%
-  gather(val_n, value, -release_id) %>%
+  gather(val_n, x, -release_id) %>%
   separate(val_n, into = c('measure', 'time_n', 'tv')) %>%
   select(-tv)
 
@@ -34,9 +35,10 @@ dpp_lp_long <- dpp_lp_long_t %>%
   inner_join(
     dpp_lp %>%
       transmute(release_id, 
+                id = as.numeric(as.factor(release_id)),
              y = glucose_2yr_value,
              y_diff = glucose_2yr_value - glucose_baseline_value)
 ) %>%
-  filter(!is.na(time))
+  filter(!is.na(tt))
 
 write_csv(dpp_lp_long, here('data', 'dpp_long_data_clean.csv'))
