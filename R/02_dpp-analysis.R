6dbd2cf68c5f4afd707c3ff1c481d769a93ec05a
Agniel, Denis <dagniel@rand.org>
2019-02-21 17:27:35 -0600

    Update analyses.

--- R/02_dpp-analysis.R
+++ R/02_dpp-analysis.R
@@ -17,9 +17,11 @@ library(tidyverse)
 dpp <- read_csv(here('data', 'dpp_long_data_clean.csv'))
 
 trt_ds <- dpp %>%
-  filter(a == 1)
+  filter(a == 1) %>%
+  mutate(id = factor(id))
 ctrl_ds <- dpp %>%
-  filter(a == 0)
+  filter(a == 0) %>%
+  mutate(id = factor(id))
 
 n_trt <- trt_ds %>%
   summarise(n_trt = length(unique(id))) %>%
@@ -37,17 +39,28 @@ y_c <- ctrl_ds %>%
   unique %>%
   pull(y)
 
-c(trt_xhat_wide, ctrl_xhat_wide, trt_scores, ctrl_scores) %<-%
-  presmooth_data(obs_data = dpp, n_trt = n_trt, n_ctrl = n_ctrl)
+gam_ctrl <- mgcv::gam(x ~ s(tt) + s(tt, by = id, bs = 're'), data = ctrl_ds)
+gam_trt <- mgcv::gam(x ~ s(tt) + s(tt, by = id, bs = 're'), data = trt_ds) 
 
-smoothed_dpp <- as_tibble(trt_xhat_wide, rownames = 'id') %>%
-  mutate(a = 1) %>%
-  full_join(
-    as_tibble(ctrl_xhat_wide, rownames = 'id') %>%
-      mutate(a = 0)
-  ) %>%
-  gather(tt, x, -id, -a) %>%
-  mutate(tt = as.numeric(tt))
+smoothed_tt <- tibble(
+  id = rep(unique(dpp$id), each = 101),
+  tt = rep(seq(0, max(dpp$tt), length = 101), length(unique(dpp$id)))
+) %>%
+  inner_join(
+    dpp %>% select(id, a) %>% unique
+  )
+smoothed_ctrl <- smoothed_tt %>%
+  filter(a == 0)
+smoothed_ctrl <- smoothed_ctrl %>%
+  mutate(xhat = predict(gam_ctrl, newdata = smoothed_ctrl))
+smoothed_trt <- smoothed_tt %>%
+  filter(a == 1)
+smoothed_trt <- smoothed_trt %>%
+  mutate(xhat = predict(gam_trt, newdata = smoothed_trt))
+smoothed_dpp <- bind_rows(
+  list(smoothed_ctrl, smoothed_trt)
+)
+  
                   
 
 ggplot(dpp, aes(x = tt, y = x, group = id)) +
@@ -57,7 +70,7 @@ ggplot(dpp, aes(x = tt, y = x, group = id)) +
   theme_bw() +
   ggtitle('Raw data by treatment group')
 
-ggplot(smoothed_dpp, aes(x = tt, y = x, group = id)) +
+ggplot(smoothed_dpp, aes(x = tt, y = xhat, group = id)) +
   geom_line(alpha = 0.5) +
   facet_wrap(~ a) +
   theme_bw() +
