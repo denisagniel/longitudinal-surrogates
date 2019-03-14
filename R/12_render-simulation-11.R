7cf3e7f5406cbb9cdc541756dedb8b804cd0ec74
Agniel, Denis <dagniel@rand.org>
2019-03-13 20:27:53 -0500

    Lengthen sims.

--- R/12_render-simulation-11.R
+++ R/12_render-simulation-11.R
@@ -2,7 +2,7 @@ library(here)
 options(
   clustermq.defaults = list(ptn="medium",
                             log_file="Rout/log%a.log",
-                            time_amt = "24:00:00"
+                            time_amt = "36:00:00"
   )
 )
 rmarkdown::render(here(
