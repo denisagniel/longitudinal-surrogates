f2d36b9024fe2e85c4583a88b79a3f13845e62d2
Agniel, Denis <dagniel@rand.org>
2019-04-30 15:08:35 -0500

    Launch full sims.

--- R/20_nonlinear-and-linear-sim.R
+++ R/20_nonlinear-and-linear-sim.R
@@ -12,10 +12,10 @@ tmpdir <- '/n/data1/hms/dbmi/zaklab/dma12/longitudinal-surrogates/tmp/'
 fs::dir_create(tmpdir)
 
 
-longsurr::lsa_sim(n = 50, n_i = 10, m = 'nonlinear', s_y = 1, s_x = 1, delta = 15, B = 0, run = 12, tmpdir)
+# longsurr::lsa_sim(n = 50, n_i = 10, m = 'nonlinear', s_y = 1, s_x = 1, delta = 15, B = 0, run = 12, tmpdir)
 
 sim_parameters <- expand.grid(
-  run = 1:5,
+  run = 1:1000,
   n = c(50, 250, 500, 1000),
   n_i = c(5, 10, 25),
   m = c('linear', 'nonlinear'),
@@ -27,9 +27,9 @@ sim_parameters <- expand.grid(
   filter(n == 50 | (B == 0))
 
 options(
-  clustermq.defaults = list(ptn="short",
+  clustermq.defaults = list(ptn="medium",
                             log_file="Rout/log%a.log",
-                            time_amt = "12:00:00"
+                            time_amt = "36:00:00"
   )
 )
 
@@ -43,7 +43,7 @@ sim_res <- Q(lsa_sim,
              delta = sim_parameters$delta,
              run = sim_parameters$run,
              const = list(tmpdir = tmpdir),
-             n_jobs = 20,
+             n_jobs = 100,
              memory = 2000,
              fail_on_error = FALSE
 )
