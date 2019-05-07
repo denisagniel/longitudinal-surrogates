3ca839ec1abd335464159b52fd8be51dfe8e5e4e
Agniel, Denis <dagniel@rand.org>
2019-05-07 09:19:35 -0500

    Do boot sims.

---
+++ R/30_boot-sim.R
@@ -0,0 +1,53 @@
+library(dplyr)
+library(clustermq)
+library(here)
+library(purrr)
+library(readr)
+library(glue)
+library(ggplot2)
+remotes::install_github('denisagniel/longsurr')
+library(longsurr)
+
+tmpdir <- '/n/data1/hms/dbmi/zaklab/dma12/longitudinal-surrogates/tmp/'
+fs::dir_create(tmpdir)
+
+
+# longsurr::lsa_sim(n = 50, n_i = 10, m = 'nonlinear', s_y = 1, s_x = 1, delta = 15, B = 0, run = 12, tmpdir)
+
+sim_parameters <- expand.grid(
+  run = 1:3,
+  n = c(50, 250),
+  n_i = c(5, 10, 25),
+  m = c('linear', 'nonlinear'),
+  s_y = 1,
+  s_x = 1,
+  delta = c(5, 25),
+  B = 250
+)
+
+options(
+  clustermq.defaults = list(ptn="short",
+                            log_file="Rout/log%a.log",
+                            time_amt = "12:00:00"
+  )
+)
+
+sim_res <- Q(lsa_sim, 
+             n = sim_parameters$n,
+             n_i = sim_parameters$n_i,
+             m = sim_parameters$m,
+             s_y = sim_parameters$s_y,
+             s_x = sim_parameters$s_x,
+             B = sim_parameters$B,
+             delta = sim_parameters$delta,
+             run = sim_parameters$run,
+             const = list(tmpdir = tmpdir),
+             n_jobs = 10,
+             memory = 800,
+             fail_on_error = FALSE
+)
+saveRDS(sim_res, 
+        here('results/boot-sims.rds'))
+fs::dir_delete(tmpdir)
+
+sessionInfo()
