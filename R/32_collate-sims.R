7b35ba93c510a720a78667ce4e9b9257fb82ca97
Agniel, Denis <dagniel@rand.org>
2019-05-14 12:09:45 -0500

    Pull in boot sims and make plots.

---
+++ R/32_collate-sims.R
@@ -0,0 +1,55 @@
+#' ---
+#' title: "Collect simulations from tmp"
+#' output: github_document
+#' ---
+#' 
+library(knitr)z
+opts_chunk$set(warning = FALSE, message = FALSE, cache = FALSE, fig.width = 7, fig.height = 7)
+
+#'
+library(dplyr)
+library(here)
+library(glue)
+library(purrr)
+sim_parameters <- expand.grid(
+  run = 1:1000,
+  n = 50,
+  n_i = c(5, 10, 25),
+  m = c('linear', 'nonlinear'),
+  s_y = 1,
+  s_x = 1,
+  delta = c(5, 25),
+  B = 250
+)
+tmpdir <- '/n/data1/hms/dbmi/zaklab/dma12/longitudinal-surrogates/tmp/'
+
+sim_res <- map(1:nrow(sim_parameters), function(i) {
+  # browser()
+  sp <- sim_parameters[i,]
+  n <- sp$n
+  n_i <- sp$n_i
+  m <- sp$m
+  s_x <- sp$s_x
+  s_y <- sp$s_y
+  delta <- sp$delta
+  B <- sp$B
+  run <- sp$run
+  
+  this_sim <- try(readRDS(glue('{tmpdir}/res_n{n}_ni{n_i}_m-{m}_sy{s_y}_sx{s_x}_delta{delta}_B{B}_{run}.rds')
+  ))
+  if (class(this_sim) == 'try-error') {
+    return(NULL)
+  } else return(this_sim %>%
+                  mutate(n = n,
+                         n_i = n_i,
+                         m = m,
+                         s_x = s_x,
+                         s_y = s_y,
+                         delta = delta,
+                         B = B,
+                         run = run))
+})
+
+final_sims <- sim_res %>% bind_rows
+readr::write_csv(final_sims, 
+                 here('results/boot-sim-results-prelim.csv'))
---
+++ R/33_scp-results.R
@@ -0,0 +1,2 @@
+scp   dma12@transfer.rc.hms.harvard.edu:~/longitudinal-surrogates/results/boot-sim-results-prelim.csv ~/rprojects/parast_longitudinal-surrogate-markers/longitudinal-surrogates/results
+
